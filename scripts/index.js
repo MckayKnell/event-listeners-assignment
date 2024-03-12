ed49c826e915039776129bfb0b9e62f7840ba77b
McKay Knell <mckay@devpipeline.com>
2024-03-11 21:33:12 -0700

    updated changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
 const randomColor = () => {
-  let number = (Math.random() * 0xfffff * 1000000).toString(16);
-  return "#" + number.slice(0, 6);
+  const hexNum = (Math.random() * 0xfffff * 1000000).toString(16);
+  return "#" + hexNum.slice(0, 6);
 };
 
-console.log(randomColor());
 button = document.querySelector(".color-button");
 button.addEventListener("click", function changeButton() {
   button.style["backgroundColor"] = randomColor();
   button.style["color"] = randomColor();
+  console.log(randomColor());
 });
 
 //challenge 2
-let input = document.getElementById("in");
-let out = document.getElementById("out");
+const input = document.getElementById("in");
+const out = document.getElementById("out");
 
 input.onkeyup = () => {
   if (input.value.length < 50) {
-    out.innerHTML = input.value;
+    out.innerText = input.value;
   }
 };
 
 function deleteButton() {
   input.value = "";
-  out.innerHTML = "Challenge 2";
+  out.innerText = "Challenge 2";
 }
